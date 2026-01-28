d86a7a16081b30f68d0f44e466ef99a6d4ef9fee
Niccolò Podlasinski <niccolo@iMac-di-Niccolo.local>
2026-01-28 11:06:31 +0000

    install pg and add db-test endpoint

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("CRM backend running on port " + PORT);
 });
+// STEP 1 - Database test
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ dbTime: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "DB connection failed" });
+  }
+});
 
